def571cf5a4070e10db3998ec79af73a882a5c0b
Александр <awakentrue@outlook.com>
2021-08-20 00:42:49 +0500

    Resolved tasks numbered 2, 3, 4

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    const mathSigns = [
+      { sing: 'sum', calcFunc: (v1, v2) => v1 + v2, icon: '➕' },
+      { sing: 'subtract', calcFunc: (v1, v2) => v1 - v2, icon: '➖' },
+      { sing: 'multiply', calcFunc: (v1, v2) => v1 * v2, icon: '✖' },
+      { sing: 'divide', calcFunc: (v1, v2) => v1 / v2, icon: '➗' },
+    ];
+
+    return {
+      firstValue: 0,
+      secondValue: 0,
+      mathSigns: mathSigns,
+      selectedMathSing: mathSigns[0],
+    };
+  },
+
+  computed: {
+    result() {
+      const selectedCalcFunc = this.selectedMathSing.calcFunc;
+      return selectedCalcFunc(this.firstValue, this.secondValue);
+    },
+  },
+});
+
+app.mount('#app');
